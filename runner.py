773a7be59d4046585b7c36d2ba22008677850eb6
danielacojocaru07 <120244122+danielacojocaru07@users.noreply.github.com>
2024-06-28 02:22:00 +0300

    Update runner.py

--- runner.py
+++ runner.py
@@ -1,6 +1,7 @@
 from timeit import default_timer as timer
 import random
 
+# Add here the sorting algorithm
 def shellSort(x):
     # Rearrange elements at each n/2, n/4, n/8, ... intervals
     n = len(x)
@@ -18,7 +19,7 @@ def shellSort(x):
     return x
 
 x = []
-for i in range(100000000):
+for i in range(100000000): # set the number of elements in the set
     x.append(random.randint(-100, 100))
 
 start = timer()
